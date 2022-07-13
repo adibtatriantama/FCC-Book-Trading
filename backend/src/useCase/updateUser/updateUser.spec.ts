aa51c330e6f4be17709e22c6daa8fa30b991433f
Adibta Triantama <adibtatriantama@gmail.com>
2022-07-13 11:15:03 +0700

    feat: create updateUser use case

---
+++ backend/src/useCase/updateUser/updateUser.spec.ts
@@ -0,0 +1,104 @@
+import { NOT_FOUND } from 'src/constant';
+import { Result } from 'src/core/result';
+import { UnexpectedError } from 'src/core/useCaseError';
+import { User } from 'src/domain/user';
+import { UserRepo } from 'src/repo/userRepo';
+import { UpdateUser, UpdateUserRequest, UserNotFoundError } from './updateUser';
+
+let useCase: UpdateUser;
+let mockUserRepo: UserRepo;
+
+const dummyRequest: UpdateUserRequest = {
+  id: 'testerId',
+  nickname: 'tester',
+  address: {
+    state: 'state',
+    city: 'city',
+  },
+};
+
+const dummyUser = User.create(
+  {
+    nickname: 'tester',
+    email: 'tester@mail.com',
+    address: {
+      state: 'state',
+      city: 'city',
+    },
+  },
+  'testerId',
+);
+
+describe('UpdateUser', () => {
+  beforeEach(() => {
+    mockUserRepo = {
+      findOneById: jest.fn().mockResolvedValue(Result.ok(dummyUser)),
+      save: jest.fn().mockResolvedValue(Result.ok()),
+    };
+    useCase = new UpdateUser(mockUserRepo);
+  });
+
+  it('should return dto', async () => {
+    const response = await useCase.execute(dummyRequest);
+
+    expect(response.isRight()).toBe(true);
+    expect(response.value).toBeDefined();
+  });
+
+  it('should save the user', async () => {
+    await useCase.execute(dummyRequest);
+
+    expect(mockUserRepo.save).toHaveBeenCalled();
+  });
+
+  describe('when user is not found', () => {
+    beforeEach(() => {
+      mockUserRepo = {
+        findOneById: jest.fn().mockResolvedValue(Result.fail(NOT_FOUND)),
+        save: jest.fn(),
+      };
+      useCase = new UpdateUser(mockUserRepo);
+    });
+
+    it('should return UserNotFoundError', async () => {
+      const response = await useCase.execute(dummyRequest);
+
+      expect(response.isLeft()).toBe(true);
+      expect(response.value.constructor).toBe(UserNotFoundError);
+    });
+  });
+
+  describe('when unable to save', () => {
+    beforeEach(() => {
+      mockUserRepo = {
+        findOneById: jest.fn().mockResolvedValue(Result.ok(dummyUser)),
+        save: jest.fn().mockResolvedValue(Result.fail(NOT_FOUND)),
+      };
+      useCase = new UpdateUser(mockUserRepo);
+    });
+
+    it('should return UnexpectedError', async () => {
+      const response = await useCase.execute(dummyRequest);
+
+      expect(response.isLeft()).toBe(true);
+      expect(response.value.constructor).toBe(UnexpectedError);
+    });
+  });
+
+  describe('when unable to check if user with same id is already exist', () => {
+    beforeEach(() => {
+      mockUserRepo = {
+        findOneById: jest.fn().mockResolvedValue(Result.fail('other error')),
+        save: jest.fn(),
+      };
+      useCase = new UpdateUser(mockUserRepo);
+    });
+
+    it('should return UnexpectedError', async () => {
+      const response = await useCase.execute(dummyRequest);
+
+      expect(response.isLeft()).toBe(true);
+      expect(response.value.constructor).toBe(UnexpectedError);
+    });
+  });
+});
---
+++ backend/src/useCase/updateUser/updateUser.ts
@@ -0,0 +1,56 @@
+import { NOT_FOUND } from 'src/constant';
+import { Either, left, right } from 'src/core/either';
+import { UseCase } from 'src/core/useCase';
+import { UseCaseError, UnexpectedError } from 'src/core/useCaseError';
+import { User, UserProps } from 'src/domain/user';
+import { UserDto } from 'src/dto/userDto';
+import { UserMapper } from 'src/mapper/userMapper';
+import { UserRepo } from 'src/repo/userRepo';
+
+export type UpdateUserRequest = Omit<UserProps, 'email'> & { id: string };
+
+export type UpdateUserResponse = Either<UseCaseError, UserDto>;
+
+export class UserNotFoundError extends UseCaseError {
+  constructor() {
+    super('User not found');
+  }
+}
+
+export class UpdateUser
+  implements UseCase<UpdateUserRequest, UpdateUserResponse>
+{
+  constructor(private readonly userRepo: UserRepo) {}
+
+  async execute(request: UpdateUserRequest): Promise<UpdateUserResponse> {
+    const findExistingUserResult = await this.userRepo.findOneById(request.id);
+
+    if (findExistingUserResult.isFailure) {
+      switch (findExistingUserResult.getErrorValue()) {
+        case NOT_FOUND:
+          return left(new UserNotFoundError());
+        default:
+          return left(new UnexpectedError());
+      }
+    }
+
+    const existingUser = findExistingUserResult.getValue();
+
+    const user = User.create(
+      {
+        nickname: request.nickname ?? existingUser.nickname,
+        email: existingUser.email,
+        address: request.address ?? existingUser.address,
+      },
+      request.id,
+    ).getValue();
+
+    const saveResult = await this.userRepo.save(user);
+
+    if (saveResult.isFailure) {
+      return left(new UnexpectedError());
+    }
+
+    return right(UserMapper.toDto(user));
+  }
+}
