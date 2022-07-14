ebbca53ee7431b318b6b8dea3d1402f4f9d042b0
Adibta Triantama <adibtatriantama@gmail.com>
2022-07-14 12:48:12 +0700

    feat: create acceptTrade use case

--- backend/src/repo/tradeRepo.ts
+++ backend/src/repo/tradeRepo.ts
@@ -2,5 +2,7 @@ import { Result } from 'src/core/result';
 import { Trade } from 'src/domain/trade';
 
 export interface TradeRepo {
+  findById(tradeId: string): Promise<Result<Trade>>;
+
   save(trade: Trade): Promise<Result<Trade>>;
 }
---
+++ backend/src/useCase/trade/acceptTrade/acceptTrade.ts
@@ -0,0 +1,64 @@
+import { NOT_FOUND } from 'src/constant';
+import { Either, left, right } from 'src/core/either';
+import { UseCase } from 'src/core/useCase';
+import {
+  EntityNotFoundError,
+  UnexpectedError,
+  UseCaseError,
+} from 'src/core/useCaseError';
+import { Trade } from 'src/domain/trade';
+import { TradeDto } from 'src/dto/tradeDto';
+import { TradeMapper } from 'src/mapper/tradeMapper';
+import { BookRepo } from 'src/repo/bookRepo';
+import { TradeRepo } from 'src/repo/tradeRepo';
+import { UserRepo } from 'src/repo/userRepo';
+
+export type AcceptTradeRequest = {
+  userId: string;
+  tradeId: string;
+};
+
+export class UnableToAcceptTradeError extends UseCaseError {
+  constructor(message: string) {
+    super('Unable to accept trade, ' + message);
+  }
+}
+
+export type AcceptTradeResponse = Either<UseCaseError, TradeDto>;
+
+export class AcceptTrade
+  implements UseCase<AcceptTradeRequest, AcceptTradeResponse>
+{
+  constructor(private readonly tradeRepo: TradeRepo) {}
+
+  async execute(request: AcceptTradeRequest): Promise<AcceptTradeResponse> {
+    const getTradeResult = await this.tradeRepo.findById(request.tradeId);
+
+    if (getTradeResult.isFailure) {
+      switch (getTradeResult.getErrorValue()) {
+        case NOT_FOUND:
+          return left(new EntityNotFoundError());
+        default:
+          return left(new UnexpectedError());
+      }
+    }
+
+    const trade = getTradeResult.getValue();
+
+    const acceptResult = trade.accept();
+
+    if (acceptResult.isFailure) {
+      return left(new UnableToAcceptTradeError(acceptResult.getErrorValue()));
+    }
+
+    const saveResult = await this.tradeRepo.save(trade);
+
+    if (saveResult.isFailure) {
+      return left(new UnexpectedError());
+    }
+
+    const savedTrades = saveResult.getValue();
+
+    return right(TradeMapper.toDto(savedTrades));
+  }
+}
