1307ba9bcbce5cf420f76f0e83f6673398915898
Adibta Triantama <adibtatriantama@gmail.com>
2022-07-14 13:02:29 +0700

    feat: create rejectTrade use case

---
+++ backend/src/useCase/trade/rejectTrade/rejectTrade.spec.ts
@@ -0,0 +1,201 @@
+import { NOT_FOUND } from 'src/constant';
+import { Result } from 'src/core/result';
+import { EntityNotFoundError, UnexpectedError } from 'src/core/useCaseError';
+import { Book, BookProps } from 'src/domain/book';
+import { Trade } from 'src/domain/trade';
+import { User } from 'src/domain/user';
+import { TradeRepo } from 'src/repo/tradeRepo';
+import {
+  RejectTrade,
+  RejectTradeRequest,
+  UnableToRejectTradeError,
+} from './rejectTrade';
+
+const dummyOwner = User.create(
+  { nickname: 'owner', email: 'owner@mail.com' },
+  'ownerId',
+).getValue();
+
+const dummyTrader = User.create(
+  { nickname: 'trader', email: 'trader@mail.com' },
+  'traderId',
+).getValue();
+
+const somebodyElse = User.create(
+  { nickname: 'unknown', email: 'unknown@mail.com' },
+  'unknownId',
+).getValue();
+
+let useCase: RejectTrade;
+let mockTradeRepo: TradeRepo;
+
+let dummyBook1: Book;
+let dummyBook2: Book;
+let dummyTrade: Trade;
+
+let request: RejectTradeRequest;
+
+const buildMockTradeRepo = (params?: Partial<TradeRepo>) => {
+  return {
+    save: params?.save ?? jest.fn(),
+    findById: params?.findById ?? jest.fn(),
+  };
+};
+
+const buildBook = (params?: Partial<BookProps>, id?: string) => {
+  return Book.create(
+    {
+      title: params.title ?? 'title',
+      author: params.author ?? 'author',
+      description: params.description ?? 'descr',
+      owner: params.owner ?? dummyOwner,
+    },
+    id,
+  ).getValue();
+};
+
+beforeEach(() => {
+  dummyBook1 = buildBook({ title: 'dummyBook 1', owner: dummyOwner }, 'book1');
+  dummyBook2 = buildBook({ title: 'dummyBook 2', owner: dummyTrader }, 'book2');
+  dummyTrade = Trade.create({
+    owner: dummyOwner,
+    trader: dummyTrader,
+    ownerBooks: [dummyBook1],
+    traderBooks: [dummyBook2],
+    status: 'pending',
+  }).getValue();
+
+  mockTradeRepo = buildMockTradeRepo({
+    save: jest.fn().mockResolvedValue(Result.ok(dummyTrade)),
+    findById: jest.fn().mockResolvedValue(Result.ok(dummyTrade)),
+  });
+
+  useCase = new RejectTrade(mockTradeRepo);
+
+  request = {
+    userId: 'ownerId',
+    tradeId: 'tradeId',
+  };
+});
+
+describe('Reject Trade', () => {
+  it('should return dto', async () => {
+    const response = await useCase.execute(request);
+
+    expect(response.isRight()).toBe(true);
+    expect(response.value).toBeDefined();
+  });
+
+  it('should save the rejected trade', async () => {
+    const spy = jest.spyOn(mockTradeRepo, 'save');
+
+    await useCase.execute(request);
+
+    expect(mockTradeRepo.save).toHaveBeenCalled();
+    expect(spy.mock.calls[0][0].status).toBe('rejected');
+  });
+
+  describe("when requested trade status isn't pending", () => {
+    beforeEach(() => {
+      dummyTrade = Trade.create({
+        owner: dummyOwner,
+        trader: dummyTrader,
+        ownerBooks: [dummyBook1],
+        traderBooks: [dummyBook2],
+        status: 'rejected',
+      }).getValue();
+
+      mockTradeRepo = buildMockTradeRepo({
+        save: jest.fn().mockResolvedValue(Result.ok(dummyTrade)),
+        findById: jest.fn().mockResolvedValue(Result.ok(dummyTrade)),
+      });
+
+      useCase = new RejectTrade(mockTradeRepo);
+    });
+
+    it('should return UnableToAcceptTradeError', async () => {
+      const response = await useCase.execute(request);
+
+      expect(response.isLeft()).toBe(true);
+      expect(response.value).toBeInstanceOf(UnableToRejectTradeError);
+    });
+  });
+
+  describe('when trade not found', () => {
+    beforeEach(() => {
+      mockTradeRepo = buildMockTradeRepo({
+        findById: jest.fn().mockResolvedValue(Result.fail(NOT_FOUND)),
+      });
+
+      useCase = new RejectTrade(mockTradeRepo);
+    });
+
+    it('should return EntityNotFoundError', async () => {
+      const response = await useCase.execute(request);
+
+      expect(response.isLeft()).toBe(true);
+      expect(response.value).toBeInstanceOf(EntityNotFoundError);
+    });
+  });
+
+  describe('when unable to find trade', () => {
+    beforeEach(() => {
+      mockTradeRepo = buildMockTradeRepo({
+        findById: jest.fn().mockResolvedValue(Result.fail('any')),
+      });
+
+      useCase = new RejectTrade(mockTradeRepo);
+    });
+
+    it('should return unexpected error', async () => {
+      const response = await useCase.execute(request);
+
+      expect(response.isLeft()).toBe(true);
+      expect(response.value).toBeInstanceOf(UnexpectedError);
+    });
+  });
+
+  describe("when user doesnt't own the trade", () => {
+    beforeEach(() => {
+      dummyTrade = Trade.create({
+        owner: somebodyElse,
+        trader: dummyTrader,
+        ownerBooks: [dummyBook1],
+        traderBooks: [dummyBook2],
+        status: 'pending',
+      }).getValue();
+
+      mockTradeRepo = buildMockTradeRepo({
+        save: jest.fn().mockResolvedValue(Result.ok(dummyTrade)),
+        findById: jest.fn().mockResolvedValue(Result.ok(dummyTrade)),
+      });
+
+      useCase = new RejectTrade(mockTradeRepo);
+    });
+
+    it('should return UnableToAcceptTradeError', async () => {
+      const response = await useCase.execute(request);
+
+      expect(response.isLeft()).toBe(true);
+      expect(response.value).toBeInstanceOf(UnableToRejectTradeError);
+    });
+  });
+
+  describe('when unable to save', () => {
+    beforeEach(() => {
+      mockTradeRepo = buildMockTradeRepo({
+        findById: jest.fn().mockResolvedValue(Result.ok(dummyTrade)),
+        save: jest.fn().mockResolvedValue(Result.fail('any')),
+      });
+
+      useCase = new RejectTrade(mockTradeRepo);
+    });
+
+    it('should return unexpected error', async () => {
+      const response = await useCase.execute(request);
+
+      expect(response.isLeft()).toBe(true);
+      expect(response.value).toBeInstanceOf(UnexpectedError);
+    });
+  });
+});
---
+++ backend/src/useCase/trade/rejectTrade/rejectTrade.ts
@@ -0,0 +1,67 @@
+import { NOT_FOUND } from 'src/constant';
+import { Either, left, right } from 'src/core/either';
+import { UseCase } from 'src/core/useCase';
+import {
+  EntityNotFoundError,
+  UnexpectedError,
+  UseCaseError,
+} from 'src/core/useCaseError';
+import { TradeDto } from 'src/dto/tradeDto';
+import { TradeMapper } from 'src/mapper/tradeMapper';
+import { TradeRepo } from 'src/repo/tradeRepo';
+
+export type RejectTradeRequest = {
+  userId: string;
+  tradeId: string;
+};
+
+export class UnableToRejectTradeError extends UseCaseError {
+  constructor(message: string) {
+    super('Unable to reject trade, ' + message);
+  }
+}
+
+export type RejectTradeResponse = Either<UseCaseError, TradeDto>;
+
+export class RejectTrade
+  implements UseCase<RejectTradeRequest, RejectTradeResponse>
+{
+  constructor(private readonly tradeRepo: TradeRepo) {}
+
+  async execute(request: RejectTradeRequest): Promise<RejectTradeResponse> {
+    const getTradeResult = await this.tradeRepo.findById(request.tradeId);
+
+    if (getTradeResult.isFailure) {
+      switch (getTradeResult.getErrorValue()) {
+        case NOT_FOUND:
+          return left(new EntityNotFoundError());
+        default:
+          return left(new UnexpectedError());
+      }
+    }
+
+    const trade = getTradeResult.getValue();
+
+    if (trade.owner.id !== request.userId) {
+      return left(
+        new UnableToRejectTradeError('You are not the owner of this trade'),
+      );
+    }
+
+    const rejectResult = trade.reject();
+
+    if (rejectResult.isFailure) {
+      return left(new UnableToRejectTradeError(rejectResult.getErrorValue()));
+    }
+
+    const saveResult = await this.tradeRepo.save(trade);
+
+    if (saveResult.isFailure) {
+      return left(new UnexpectedError());
+    }
+
+    const savedTrades = saveResult.getValue();
+
+    return right(TradeMapper.toDto(savedTrades));
+  }
+}
