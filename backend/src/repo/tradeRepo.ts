1d168d137b3bbc41cf13b9edecc5e339975f9cf6
Adibta Triantama <adibtatriantama@gmail.com>
2022-07-14 14:07:51 +0700

    fix: add missing function

--- backend/src/repo/tradeRepo.ts
+++ backend/src/repo/tradeRepo.ts
@@ -5,6 +5,7 @@ export interface TradeRepo {
   findById(tradeId: string): Promise<Result<Trade>>;
   findTradeByOwnerId(ownerId: string): Promise<Result<Trade[]>>;
   findTradeByTraderId(traderId: string): Promise<Result<Trade[]>>;
+  findTradeByBookId(bookId: string): Promise<Result<Trade[]>>;
 
   save(trade: Trade): Promise<Result<Trade>>;
   remove(trade: Trade): Promise<Result<void>>;
